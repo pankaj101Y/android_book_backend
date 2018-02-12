be038abf4b4ad9e1bb2a0ef22b1d0a7978dc3675
pankaj101Y <pnkcomm2u@gmail.com>
2018-02-12 17:43:43 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+const port=process.env.port||3000;
+
+app.post("/register",function(req,res){
+    console.log(req);
+    res.send("success");
+});
+
+
+app.listen(port,function(){
+    console.log(`server running on ${port}`);
+});
