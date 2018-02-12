b217c6f28bdecd48ecfe967034d49a0aae7fc62f
pankaj101Y <pnkcomm2u@gmail.com>
2018-02-12 18:14:56 +0530

    addBook

--- app.js
+++ app.js
@@ -3,9 +3,33 @@ const app = express();
 
 const port=process.env.PORT||3000;
 
+/*
+* inputs:{
+*   email:"",
+*   password:""
+*   }
+*
+*   output: create mongoDB id
+* */
 app.post("/register",function(req,res){
     console.log(req);
-    res.send("success");
+    res.send("id");
+});
+
+
+/**
+ *inputs:{
+ * name:"",
+ * author:"",
+ * tag:""
+ * }
+ *
+ * output: mongo id of book added
+ */
+
+app.post("/addBook",function(req,res){
+    console.log(req);
+    res.send("id");
 });
 
 
